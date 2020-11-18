6506a6f7281d47d15e76539e5f243566c5897ec8
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-11-18 22:15:01 +0200

    Add files via upload

---
+++ Problema 4.py
@@ -0,0 +1,7 @@
+from fractions import Fraction
+a=int(input('Introduceti numarul a='))
+b=int(input('Introduceti numarul b='))
+c=int(input('Introduceti numarul c='))
+d=int(input('Introduceti numarul d='))
+print('Suma este=',Fraction(a,b)+Fraction(c,d))
+print('Produsul este=',Fraction(a,b)*Fraction(c,d))
