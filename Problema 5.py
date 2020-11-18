430429fe99c19e43121940802e7e4d2acca4ea48
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-11-18 22:15:18 +0200

    Add files via upload

---
+++ Problema 5.py
@@ -0,0 +1,26 @@
+x=int(input('Introduceti anul='))
+x>1000
+if x%12==0:
+    print('Dragon')
+if x%12==1:
+    print('Sarpe')
+if x%12==2:
+    print('Cal')
+if x%12==3:
+    print('Oaie')
+if x%12==4:
+    print('Maimuta')
+if x%12==5:
+    print('Cocos')
+if x%12==6:
+    print('Ciine')
+if x%12==7:
+    print('Porc')
+if x%12==8:
+    print('Sobolan')
+if x%12==9:
+    print('Bou')
+if x%12==10:
+    print('Tigru')
+if x%12==11:
+    print('Iepure')
