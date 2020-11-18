707f65d9e49bb62d2acaed205535c96615c3bb19
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-11-18 17:49:11 +0200

    Add files via upload

---
+++ Problema 1.py
@@ -0,0 +1,11 @@
+a=int(input('Introduceti numarul a='))
+if a==28:
+    print('Februarie')
+if a==29:
+    print('Februarie,an bisect')
+if a==30:
+    print('Aprilie, Iunie, Septembrie, Noiembrie')
+if a==31:
+    print('Decembrie, Martie, Mai, Iulie, August, Octombrie')
+if a<28 or  a>31:
+    print('Studiaza stiintele naturii')
